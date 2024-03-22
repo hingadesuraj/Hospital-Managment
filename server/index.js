c3faa754e956ea94d2157f7bc44dfcdba7961ab0
Suraj <hingadesuraj@gmail.com>
2024-03-22 16:16:55 +0530

    set cors policy

--- server/index.js
+++ server/index.js
@@ -1,16 +1,26 @@
 import express from "express";
 import connectToDb from './db.js'
 import {config} from 'dotenv'
+import cors from 'cors'
 
 const app = express();
 // setup dotenv file
 config({path:"./config/config.env"})
- 
 connectToDb();
+
+// middleware
+// cors policy only access backend for frontend and dashboard url with request
+app.use(cors({
+    origin:[process.env.FRONTEND_URL,process.env.DASHBOARD_URL],
+    methods:["GET","POST","PUT","DELETE"],
+    credentials:true
+}))
+
 
 app.get("/", (req, res) => {
   res.send("Server is running...!");
 });
+
 
 app.listen(process.env.PORT, () => {
   console.log("Server Is running on Port http://localhost:" + process.env.PORT);
